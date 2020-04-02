3915bf4e4d46d6fa1dc909ecc84e5d86e7ad40a0
SebastianSalasDCI <sebastian.salas.laurens@digitalcareerinstitute.org>
2020-04-02 03:57:53 +0200

    Updates

---
+++ precache-manifest.fb79bc8b1533426986b9b44867224c4c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce2692b0f4980e055c4ef9b5807aeb4",
+    "url": "/letter/index.html"
+  },
+  {
+    "revision": "37779874c47f2a183a37",
+    "url": "/letter/static/css/2.596724bf.chunk.css"
+  },
+  {
+    "revision": "c50a7d29b1599bde24b0",
+    "url": "/letter/static/css/main.28799cfc.chunk.css"
+  },
+  {
+    "revision": "37779874c47f2a183a37",
+    "url": "/letter/static/js/2.b967d4f0.chunk.js"
+  },
+  {
+    "revision": "bc1a73d08d8433691746793d8481dea3",
+    "url": "/letter/static/js/2.b967d4f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c50a7d29b1599bde24b0",
+    "url": "/letter/static/js/main.0d9ef6e2.chunk.js"
+  },
+  {
+    "revision": "077e570009a69f095cfe",
+    "url": "/letter/static/js/runtime-main.34f44951.js"
+  },
+  {
+    "revision": "8d3142452d4ee9b6e9eebaa6c25aba43",
+    "url": "/letter/static/media/berlin.8d314245.jpg"
+  },
+  {
+    "revision": "52793a0b2219f8f54100279e5a7812bd",
+    "url": "/letter/static/media/blankenese2.52793a0b.jpg"
+  },
+  {
+    "revision": "cd351407580f15c94fb79ae3bffc3304",
+    "url": "/letter/static/media/ctg2.cd351407.jpg"
+  },
+  {
+    "revision": "b5210704f085407e3c74353c30e5e713",
+    "url": "/letter/static/media/cute2.b5210704.jpg"
+  },
+  {
+    "revision": "29473fab449e17185fc50edcd7a1426a",
+    "url": "/letter/static/media/dance2.29473fab.jpeg"
+  },
+  {
+    "revision": "fcc284b55a22ef7cabb31ce29ac1ff9f",
+    "url": "/letter/static/media/dreams2.fcc284b5.jpg"
+  },
+  {
+    "revision": "44e53ff864881bac2dc099323ec2c79b",
+    "url": "/letter/static/media/grad2.44e53ff8.jpeg"
+  },
+  {
+    "revision": "39929f506740e14d310a9297d0685a39",
+    "url": "/letter/static/media/hamburg2.39929f50.jpg"
+  },
+  {
+    "revision": "27bbc125c7ced0cdaa60e7d4c3b02a4b",
+    "url": "/letter/static/media/height2.27bbc125.jpg"
+  },
+  {
+    "revision": "b0a3b6fb52ed106d2796f0e21e04be88",
+    "url": "/letter/static/media/hot2.b0a3b6fb.jpeg"
+  },
+  {
+    "revision": "553bd5fecc33b00e05e505e1366d8e3b",
+    "url": "/letter/static/media/lisboa22.553bd5fe.jpg"
+  },
+  {
+    "revision": "34e70512de57efb88ef98e55194d02dd",
+    "url": "/letter/static/media/madrid.34e70512.jpg"
+  },
+  {
+    "revision": "7ae290e8063dfb04294d6f80bda39135",
+    "url": "/letter/static/media/persev.7ae290e8.jpg"
+  },
+  {
+    "revision": "3cf85130a5552c95105c99e5b4cff72c",
+    "url": "/letter/static/media/prof2.3cf85130.jpg"
+  },
+  {
+    "revision": "9f004f932d9997a8f43e3f0f45faeeb7",
+    "url": "/letter/static/media/smile2.9f004f93.jpg"
+  },
+  {
+    "revision": "afa9cb472954737afad4735f607b8806",
+    "url": "/letter/static/media/snap.afa9cb47.mp4"
+  },
+  {
+    "revision": "cf611f10fe45b5e38b9d2ccb9000d5ac",
+    "url": "/letter/static/media/style2.cf611f10.jpg"
+  },
+  {
+    "revision": "f6d04468cc39eb0fb1a723cfb4abc1fb",
+    "url": "/letter/static/media/t2.f6d04468.jpg"
+  },
+  {
+    "revision": "227c6b1662fe08e3489089dd5c5b5b5a",
+    "url": "/letter/static/media/tayrona2.227c6b16.jpg"
+  },
+  {
+    "revision": "d47331cb1ab993f0e134e006253e1aac",
+    "url": "/letter/static/media/test3.d47331cb.jpg"
+  },
+  {
+    "revision": "2f16e49c148634e4a2ba40a461d59713",
+    "url": "/letter/static/media/test5.2f16e49c.jpg"
+  }
+]);
